d6980f9eb14bb470841b659babb816b11a12b3e0
= <=>
2022-10-14 15:25:09 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './styles/global.css';
+export function App() {
+  
+  return (
+    <div>
+      <h1>teste</h1>
+      <button className='bg-cyan-500 px-4 py-2 rounded w-full'>Botao</button>
+    </div>
+  )
+}
+
