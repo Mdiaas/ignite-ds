c65be732e2052d52f92819c069102258a806fcaa
gdata-sdh <maugusto.diaas@gmail.com>
2022-10-14 19:56:21 -0300

    finalizacao layout de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,56 @@
+import { Envelope, Lock } from 'phosphor-react';
+import { Heading } from './components/Heading';
+import { Text } from './components/Text';
+import { TextInput } from './components/TextInput';
+import { Checkbox } from './components/Checkbox';
 import './styles/global.css';
+import { Button } from './components/Button';
 export function App() {
   
   return (
-    <div>
-      <h1>teste</h1>
-      <button className='bg-cyan-500 px-4 py-2 rounded w-full'>Botao</button>
+    <div className='w-screen h-screen bg-gray-900 flex items-center flex-col justify-center text-gray-100'>
+      <header className='flex flex-col items-center'>
+        <Heading size='lg'className='mt-4'>Ignite lab</Heading>
+
+        <Text size="lg" className='text-gray-400'>Faça login e comece a usar</Text>
+      </header>
+      <form className='flex flex-col items-stretch w-full max-w-sm mt-10 gap-4'>
+          <label htmlFor="email" className='flex flex-col gap-3'>
+            <Text className='font-semibold'>Endereço de e-mail</Text>
+            <TextInput.Root>
+              <TextInput.Icon>
+                <Envelope />
+              </TextInput.Icon >
+              <TextInput.Input id='email' type='email' placeholder='Digite seu e-mail' />
+            </TextInput.Root>
+          </label>
+          
+          <label htmlFor="password" className='flex flex-col gap-3'>
+            <Text className='font-semibold'>Senha</Text>
+            <TextInput.Root>
+              <TextInput.Icon>
+                <Lock />
+              </TextInput.Icon >
+              <TextInput.Input id='password' type='password' placeholder='*******'  />
+            </TextInput.Root>
+          </label>
+          
+          <label htmlFor="remember" className="flex items-center gap-2">
+            <Checkbox id='remember'/>
+            <Text size="sm" className='text-gray-200'>Lembrar de mim por 30 dias</Text>
+          </label>
+
+          <Button type='submit' className='mt-4'>Acessar plataforma</Button>
+      </form>
+
+      <footer className='flex flex-col items-center gap-4 mt-8'>
+        <Text asChild size='sm'>
+          <a className='text-gray-400 underline hover:text-gray-200 hover:cursor-pointer'>Esqueceu a senha?</a>
+        </Text>
+        <Text asChild size='sm'>
+          <a className='text-gray-400 underline hover:text-gray-200 hover:cursor-pointer'>Não possuí uma conta? Cadastre-se agora</a>
+        </Text>
+      </footer>
     </div>
   )
 }
--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -5,9 +5,10 @@ import { Slot } from '@radix-ui/react-slot';
 export interface TextProps{
     size?: 'sm' | 'md' | 'lg';
     children: ReactNode
-    asChild?: boolean
+    asChild?: boolean,
+    className?: string
 }
-export function Text({size = 'md', children, asChild }: TextProps){
+export function Text({size = 'md', children, asChild, className }: TextProps){
     const Comp = asChild ? Slot : 'span';
     return (
         <Comp className={clsx(
@@ -15,7 +16,8 @@ export function Text({size = 'md', children, asChild }: TextProps){
                 'text-xs' : size === 'sm',
                 'text-sm' : size === 'md',
                 'text-md' : size === 'lg',
-            }
+            },
+            className
         )}>{children}</Comp>
     )
 }
